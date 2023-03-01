6197035add69a5f9c532e57e9f6dee1715b20a02
TanyaMittal31 <tanyaagrawal2409@gmail.com>
2023-03-01 15:33:04 +0530

    greetings

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { Greet } from './components/Greet';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Greet />
     </div>
   );
 }
